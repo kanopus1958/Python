66f8c5bd16b1f830df5597fb0dd1d17e8826c994
kanopus1958 <85549223+kanopus1958@users.noreply.github.com>
2022-02-18 18:15:08 +0100

    commit

---
+++ allgemein/schildkroete.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# Programm     : schildkrote.py
+# Version      : 1.00
+# SW-Stand     : 18.02.2022
+# Autor        : Kanopus1958
+# Beschreibung : Tests mit Modul turtle
+
+import turtle as tu
+from rwm_mod01 import show_header
+
+G_OS = ('Windows')
+G_HEADER_1 = '# Tests mit Modul "turtle"    '
+G_HEADER_2 = '                             #'
+
+
+def _main():
+    show_header(G_HEADER_1, G_HEADER_2, __file__, G_OS)
+
+    laenge = 400
+    winkel = 170
+    n = 0
+    tu.color('red', 'yellow')
+    tu.begin_fill()
+    tu.setposition(-laenge/2, 0.0)
+    while True:
+        n += 1
+        print(n, n*winkel)
+        tu.forward(laenge)
+        tu.left(winkel)
+        if n*winkel % 360 == 0:
+            break
+    tu.end_fill()
+    tu.done()
+
+    # tu.forward(200)
+    # tu.circle(150, 90)
+    # tu.position()
+
+    # tu.done()
+
+
+if __name__ == "__main__":
+    _main()
