988553d4b1cfdc0d9dc5ae0e47032dc42213bcac
kanopus1958 <85549223+kanopus1958@users.noreply.github.com>
2022-02-18 11:35:35 +0100

    commit

--- allgemein/pythagoras.py
+++ allgemein/pythagoras.py
@@ -5,14 +5,16 @@
 # SW-Stand     : 17.02.2022
 # Autor        : Kanopus1958
 # Beschreibung : Berechnung von Tripeln die a^2 + b^2 = c^2 erfüllen
-G_OS = ('Raspbian','Debian','Windows')
-G_HEADER_1 = '# Pythagoras (Tripel die a^2 +'
-G_HEADER_2 = ' b^2 = c^2 erfüllen          #'
 
 import sys
 from rwm_mod01 import show_header
 from rwm_steuerung import color as c, position as p
 from math import sqrt
+
+G_OS = ('Raspbian', 'Debian', 'Windows')
+G_HEADER_1 = '# Pythagoras (Tripel die a^2 +'
+G_HEADER_2 = ' b^2 = c^2 erfüllen          #'
+
 
 def _main():
     show_header(G_HEADER_1, G_HEADER_2, __file__, G_OS)
@@ -22,9 +24,9 @@ def _main():
         m = int(input("Maximale Zahl? "))
         print()
         if m <= n:
-            print ("Maximale Zahl muss größer sein als erste Zahl !")
-        for a_wert in range(n,m+1):
-            for b_wert in range(a_wert,m):
+            print("Maximale Zahl muss größer sein als erste Zahl !")
+        for a_wert in range(n, m+1):
+            for b_wert in range(a_wert, m):
                 c_square = a_wert**2 + b_wert**2
                 c_wert = int(sqrt(c_square))
                 if (c_square - c_wert**2) == 0:
@@ -35,6 +37,7 @@ def _main():
         print(c.lightred, p.up)
         print(3*" ", "\n!!! Programm abgebrochen !!!\n", c.reset)
         sys.exit(False)
+
 
 if __name__ == "__main__":
     _main()
